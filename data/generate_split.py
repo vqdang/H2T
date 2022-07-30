52d2aff68aff7b8c2a1b44fa6afc5bf0ad4ab8f9
vqdang <24943262+vqdang@users.noreply.github.com>
2022-07-30 15:25:11 +0100

    UPD: track refactored split

--- data/generate_split.py
+++ data/generate_split.py
@@ -25,24 +25,7 @@ def get_cv_split(Y, num_splits=5, random_state=None, shuffle=True):
     return full_split
 
 
-# %%
-dataset_identifiers = [
-    "tcga/lung/ffpe/lusc",
-    "tcga/lung/frozen/lusc",
-    "tcga/lung/ffpe/luad",
-    "tcga/lung/frozen/luad",
-    # "tcga/breast/ffpe",
-    # "tcga/breast/frozen",
-    # "tcga/kidney/ffpe",
-    # "tcga/kidney/frozen",
-    # "cptac/luad/",
-    # "cptac/lusc/",
-]
-feature_root_dir = "/mnt/storage_0/workspace/atlas/exp_output/features/[SWAV]-[mpp=0.50]-[512-256]/temp/"
-PWD = "/mnt/storage_0/workspace/h2t/"
-
-
-def retrieve_dataset_slide_info(feature_root_dir):
+def retrieve_dataset_slide_info(feature_root_dir, dataset_identifiers):
     sample_info_per_dataset = {}
     for identifier in dataset_identifiers:
         slide_names = recur_find_ext(
@@ -60,9 +43,11 @@ def retrieve_dataset_slide_info(feature_root_dir):
             )
             slide_info_list = [[[identifier, v[0]], v[1]] for v in slide_info_list]
         elif "cptac" in identifier:
+            organ_identifier = [v for v in identifier.split("/") if len(v) > 0]
+            organ_identifier = "-".join(organ_identifier)
             slide_info_list = ds.CPTAC.retrieve_labels(
                 slide_names,
-                f"{PWD}/data/clinical/tcga-{organ_identifier}/sample.tsv",
+                f"{PWD}/data/clinical/{organ_identifier}.json",
             )
         else:
             assert False
@@ -70,15 +55,18 @@ def retrieve_dataset_slide_info(feature_root_dir):
     return sample_info_per_dataset
 
 
-def per_dataset_stratified_split(config):
-
-    def retrieve_subset(subset_info, sample_info_per_dataset):
+def per_dataset_stratified_split(config, dataset_sample_info):
+    def retrieve_subset(subset_info, dataset_sample_info):
         all_subsets = []
         for subset in subset_info:
             label_mapping = subset["labels"]
             label_mapping = {k.lower(): int(v) for k, v in label_mapping.items()}
 
-            samples = sample_info_per_dataset[subset["identifier"]]
+            samples = dataset_sample_info[subset["identifier"]]
+            # filter samples with labels that are not within selected
+            # set out (contained as keys within `label_mapping`)
+            samples = [v for v in samples if v[1] in label_mapping]
+
             sample_codes, sample_labels = list(zip(*samples))
             sample_labels = [label_mapping[v] for v in sample_labels]
             all_subsets.append([sample_codes, sample_labels])
@@ -90,7 +78,7 @@ def per_dataset_stratified_split(config):
     subset_info = config["train-valid"]
     splits = [{"train": [], "valid": []} for _ in range(num_splits)]
 
-    subset_info = retrieve_subset(subset_info, sample_info_per_dataset)
+    subset_info = retrieve_subset(subset_info, dataset_sample_info)
     for sample_codes, sample_labels in subset_info:
         subset_splits = get_cv_split(
             sample_labels, num_splits=num_splits, random_state=5, shuffle=True
@@ -105,19 +93,79 @@ def per_dataset_stratified_split(config):
     # then merge the split per each dataset to create the cv splits
     if "test" in config:
         subset_info = config["test"]
-        subset_info = retrieve_subset(subset_info, sample_info_per_dataset)
+        subset_info = retrieve_subset(subset_info, dataset_sample_info)
+        subset_info = [
+            list(zip(sample_codes, sample_labels))
+            for sample_codes, sample_labels in subset_info
+        ]
         subset_info = flatten_list(subset_info)
-        for split_idx in splits:
-            splits[split_idx]["tests"] = subset_info
-    return
+        for subsplit in splits:
+            subsplit["tests"] = subset_info
+    return splits
 
-# %%
-# a = joblib.load("splits/subset_info.dat")
+
+dataset_identifiers = [
+    "tcga/lung/ffpe/lscc",
+    "tcga/lung/frozen/lscc",
+    "tcga/lung/ffpe/luad",
+    "tcga/lung/frozen/luad",
+    "cptac/lung/luad",
+    "cptac/lung/lscc",
+    # "tcga/breast/ffpe",
+    # "tcga/breast/frozen",
+    # "tcga/kidney/ffpe",
+    # "tcga/kidney/frozen",
+]
+PWD = "/mnt/storage_0/workspace/h2t/"
+feature_root_dir = "/mnt/storage_0/workspace/h2t/experiments/local/features/[SWAV]-[mpp=0.50]-[512-256]/"
 
 num_splits = 5
-sample_info_per_dataset = retrieve_dataset_slide_info(feature_root_dir)
-config = load_yaml(f"{PWD}/data/config.yaml")
-for split_name, split_info in config.items():
-    splits = per_dataset_stratified_split(split_info)
+dataset_sample_info = retrieve_dataset_slide_info(
+    feature_root_dir, dataset_identifiers
+)
 
+# %%
+config = load_yaml(f"{PWD}/data/config.yaml")
+# for split_name, split_info in config.items():
+#     splits = per_dataset_stratified_split(split_info)
+#     break
+# %%
 print("here")
+
+old_subset_info = joblib.load("splits/subset_info.dat")
+old_mapping = {
+    "TCGA-LSCC": "tcga/lung/ffpe/lscc",
+    "TCGA-LSCC-Frozen": "tcga/lung/frozen/lscc",
+    "TCGA-LUAD": "tcga/lung/ffpe/luad",
+    "TCGA-LUAD-Frozen": "tcga/lung/frozen/luad",
+    "CPTAC-LUAD": "cptac/lung/luad",
+    "CPTAC-LSCC": "cptac/lung/lscc",
+}
+for k, v in old_mapping.items():
+    # print(len(old_subset_info[k]))
+    # print(len(dataset_sample_info[v]))
+    flags = [
+        sample[0][1] == old_subset_info[k][i][0][1]
+        for i, sample in enumerate(old_subset_info[k])
+    ]
+    print(np.sum(flags), len(old_subset_info[k]))
+
+# %%
+split_code = "[luad-lusc]_train=tcga_test=cptac"
+old_split_root = "/mnt/storage_0/workspace/h2t/data/splits/"
+old_splits = joblib.load(f"{old_split_root}/{split_code}.dat")
+
+new_splits = per_dataset_stratified_split(config[split_code])
+# %%
+for split_idx in range(num_splits):
+    old_split = old_splits[split_idx][1]
+    new_split = new_splits[split_idx]
+
+    old_ids = [v[0][1] for v in old_split["tcga-train"]]
+    new_ids = [v[0][1] for v in new_split["train"]]
+    flags = [v in old_ids for v in new_ids]
+    print(len(old_ids))
+    print(len(new_ids))
+    print(np.sum(flags))
+    print("=")
+# %%
